5122ea286e2e71b50f87e987a1e6e37416992b5b
Ivan Zheng <yvonne_7_7@hotmail.com>
2020-04-29 18:06:58 +0800

    add refitServiceResultExtension

---
+++ OAuth2Client/RefitServiceResultExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace OAuth2Client
+{
+    public static class RefitServiceResultExtension
+    {
+        public static async Task ProcessApiException(this Task task)
+        {
+            try
+            { 
+                await task.ConfigureAwait(false);
+            }
+            catch (ApiException e)
+            {
+                throw new Exception(e.Content);
+            }
+        }
+        public static async Task<T> ProcessApiException<T>(this Task<T> task)
+        {
+            try
+            {
+                return await task.ConfigureAwait(false);
+            }
+            catch (ApiException e)
+            {
+                throw new Exception(e.Content);
+            }
+        }
+    }
+}
